78a9819b26aa00a10ca1a4a38a42707f5b564cd2
Marek Wrobel <marek@wrobel.ai>
2023-01-11 12:00:51 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-testing`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
