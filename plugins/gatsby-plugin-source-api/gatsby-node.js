abe8e52926161f6e176f66ae759a71f9f3c0b19a
Marek Wrobel <marek@wrobel.ai>
2023-04-06 17:39:56 -0400

    linking test

--- plugins/gatsby-plugin-source-api/gatsby-node.js
+++ plugins/gatsby-plugin-source-api/gatsby-node.js
@@ -1,19 +1,35 @@
 // const queryString = require('query-string');
-const { initAccessToken, fetchCourses } = require("./utils.js");
+const { initAccessToken, fetchCourses, fetchCoursesMock, fetchSubjectsMock } = require("./utils.js");
 
-const originalTimestamp = '2023-03-29T16:25:59Z'
-const laterTimeStamp = '2023-03-29T20:30:59Z'
+// const originalTimestamp = '2023-03-29T16:25:59Z'
+// const laterTimeStamp = '2023-04-06T05:28:25.057Z'
 
-exports.onPreInit = async ({ cache }) => {
-  console.log('---- Loaded Sourcing Plugin ----');
-}
+// exports.onPreInit = async ({ cache }) => {
+//   console.log('---- Loaded Sourcing Plugin ----');
+// }
 
-exports.onPostBuild = async ({ cache }) => {
-  // set a timestamp at the end of the build
-  await cache.set(`last_fetched_timestamp`, Date.now())
+// exports.onPostBuild = async ({ cache }) => {
+//   // set a timestamp at the end of the build
+//   await cache.set(`last_fetched_timestamp`, Date.now())
+// }
+
+
+
+
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+
+  const typeDefs = `
+    type Course implements Node {
+      subjects: [Subject] @link(by: "course.uuid", from: "uuid")
+    }
+  `
+  createTypes(typeDefs)
 }
 
 const COURSE_NODE_TYPE = 'Course'
+const SUBJECT_NODE_TYPE = 'Subject'
 let isFirstSourceInCurrentNodeProcess = true;
 
 exports.sourceNodes = async ({
@@ -29,8 +45,6 @@ exports.sourceNodes = async ({
   const { createNode, touchNode } = actions;
 
   if (isFirstSourceInCurrentNodeProcess) {
-    // we need to loop over all nodes and touch them to tell gatsby that the 
-		// node still exists and shouldn't be garbage collected
     const allCourseNodes = getNodesByType(COURSE_NODE_TYPE);
     allCourseNodes.forEach((node) => {
       touchNode(node)
@@ -39,18 +53,28 @@ exports.sourceNodes = async ({
 
   isFirstSourceInCurrentNodeProcess = false;
 
-  // get the last timestamp from the cache
-  const lastFetchedTimestamp = laterTimeStamp //await cache.get(`last_fetched_timestamp`)
+  // FETCH ALL SUBJECTS
+  const allSubjects = fetchSubjectsMock();
 
-  await initAccessToken();
+  // FETCH ALL OR ONLY UPDATED COURSES
+  const fetchOnlyUpdatedCourses = false;
+  const allCourses = fetchCoursesMock(fetchOnlyUpdatedCourses);
 
-  reporter.info(`### Start Fetching Courses ###`)
-  const activity = reporter.activityTimer(`Fetching Courses`)
-  activity.start()
-  const allCourses = await fetchCourses(lastFetchedTimestamp);
-  activity.end()
+  // CREATE SUBJECT NODES
+  allSubjects.forEach(subject =>
+    createNode({
+      ...subject,
+      id: createNodeId(`${SUBJECT_NODE_TYPE}-${subject.uuid}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: SUBJECT_NODE_TYPE,
+        contentDigest: createContentDigest(subject),
+      },
+    })
+  ) 
 
-  // loop through data and create Gatsby nodes
+  // CREATE COURSE NODES
   allCourses.forEach(course =>
     createNode({
       ...course,
@@ -64,9 +88,80 @@ exports.sourceNodes = async ({
     })
   ) 
 
-  const courseNodes = getNodesByType(COURSE_NODE_TYPE);
-  reporter.info(`### Total Fetched Courses: ${allCourses.length} ###`)
-  reporter.info(`### Total Course Nodes: ${courseNodes.length} ###`)
-
   return
 }
+
+// const COURSE_NODE_TYPE = 'Course'
+// let isFirstSourceInCurrentNodeProcess = true;
+
+// exports.sourceNodes = async ({
+//   actions,
+//   cache,
+//   createContentDigest,
+//   createNodeId,
+//   getNodes,
+//   getNodesByType,
+//   reporter,
+// }) => {
+
+//   const { createNode, touchNode } = actions;
+
+//   if (isFirstSourceInCurrentNodeProcess) {
+//     // we need to loop over all nodes and touch them to tell gatsby that the 
+// 		// node still exists and shouldn't be garbage collected
+//     const allCourseNodes = getNodesByType(COURSE_NODE_TYPE);
+//     allCourseNodes.forEach((node) => {
+//       touchNode(node)
+//     })
+//   } 
+
+//   isFirstSourceInCurrentNodeProcess = false;
+
+//   // get the last timestamp from the cache
+//   const lastFetchedTimestamp = laterTimeStamp //await cache.get(`last_fetched_timestamp`)
+
+//   await initAccessToken();
+
+//   reporter.info(`### Start Fetching Courses ###`)
+//   const activity = reporter.activityTimer(`Fetching Courses`)
+//   activity.start()
+//   const allCourses = await fetchCourses(lastFetchedTimestamp);
+//   activity.end()
+
+//   // loop through data and create Gatsby nodes
+//   allCourses.forEach(course =>
+//     createNode({
+//       ...course,
+//       id: createNodeId(`${COURSE_NODE_TYPE}-${course.uuid}`),
+//       parent: null,
+//       children: [],
+//       internal: {
+//         type: COURSE_NODE_TYPE,
+//         contentDigest: createContentDigest(course),
+//       },
+//     })
+//   ) 
+
+//   const courseNodesBefore = getNodesByType(COURSE_NODE_TYPE);
+
+//   await createNode({
+//     ...allCourses[0],
+//     id: `qdup-${allCourses[0].nodeId}`,
+//     parent: null,
+//     children: [],
+//     internal: {
+//       type: COURSE_NODE_TYPE,
+//       contentDigest: createContentDigest(allCourses[0]),
+//     },
+//   });
+
+//   const courseNodesAfter = getNodesByType(COURSE_NODE_TYPE);
+
+
+//   // const nodeExample = JSON.stringify(courseNodes[0])
+//   // console.log(nodeExample)
+//   reporter.info(`### courseNodesBefore: ${courseNodesBefore.length} ###`)
+//   reporter.info(`### courseNodesAfter: ${courseNodesAfter.length} ###`)
+
+//   return
+// }
--- plugins/gatsby-plugin-source-api/utils.js
+++ plugins/gatsby-plugin-source-api/utils.js
@@ -59,7 +59,104 @@ const fetchCourses = async (lastFetchedTimestamp) => {
   return allCourses;
 }
 
+const allSubjects = [
+  {
+    'uuid': 'A',
+    'name': 'Subject A'
+  },
+  {
+    'uuid': 'B',
+    'name': 'Subject B'
+  },
+  {
+    'uuid': 'C',
+    'name': 'Subject C'
+  },
+]
+
+const allCourses = [
+  {
+    'uuid': 1,
+    'title': 'Course 1',
+    'subjects': [
+      {
+        'uuid': 'A',
+      },
+    ]
+  },
+  {
+    'uuid': 2,
+    'title': 'Course 2',
+    'subjects': [
+      {
+        'uuid': 'B',
+      },
+    ]
+  },
+  {
+    'uuid': 3,
+    'title': 'Course 3',
+    'subjects': [
+      {
+        'uuid': 'C',
+      },
+    ]
+  },
+  {
+    'uuid': 4,
+    'title': 'Course 4',
+    'subjects': [
+      {
+        'uuid': 'A',
+      },
+    ]
+  },
+  {
+    'uuid': 5,
+    'title': 'Course 5',
+    'subjects': [
+      {
+        'uuid': 'B',
+      },
+      {
+        'uuid': 'C',
+      },
+    ]
+  },
+]
+
+const updatedCourses = [
+  {
+    'uuid': 1,
+    'title': 'Course 1 Updated',
+    'subjects': [
+      {
+        'uuid': 'A',
+      },
+      {
+        'uuid': 'B',
+      },
+    ]
+  },
+  {
+    'uuid': 2,
+    'title': 'Course 2 Updated',
+    'subjects': [
+      {
+        'uuid': 'C',
+      },
+    ]
+  },
+]
+
+const fetchSubjectsMock = () => allSubjects;
+
+const fetchCoursesMock = (fetchOnlyUpdatedCourses = false) => fetchOnlyUpdatedCourses ? updatedCourses : allCourses;
+
+
 module.exports = {
   initAccessToken,
-  fetchCourses
+  fetchCourses,
+  fetchSubjectsMock,
+  fetchCoursesMock
 }
